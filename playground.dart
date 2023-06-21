ef05b772233508cf638bd3f7e38fcefb15545b26
mozart953 <wilsonnianopop@gmail.com>
2023-06-20 20:16:19 -0600

    method1

--- playground.dart
+++ playground.dart
@@ -1,3 +1,18 @@
-main(){
-  print("hello world");
+//dartcore
+// import 'dart:core';
+
+// main(){
+//   var firtName = 'Wilson';
+//   String lasName = 'Pop';
+
+//   print(firtName + ' ' + lasName );
+
+// }
+
+import 'dart:io';
+
+void main() {
+  stdout.write('what is your name?');
+  String? name = stdin.readLineSync();
+  print('My name is $name');
 }
