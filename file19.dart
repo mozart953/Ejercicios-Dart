d804b08190134486d8f98e04d3bee36bc79fee10
mozart953 <wilsonnianopop@gmail.com>
2023-06-23 22:22:55 -0600

    adding files about loops and functions

--- file19.dart
+++ file19.dart
@@ -1,7 +1,36 @@
 //collections
 void main(){
   //List
-  List names = ["jack", "jill"];
+  List<String> names = ["jack", "jill"];
+
+  var names2= [...names];
+  //var names2= names;
+
+  names[1]='Mark';
+
+  print(names[1]);
+
+  for(var n in names2){
+    print(n);
+  }
+
+  //set
+
+  // var halogens ={'flurine', 'chlorine','fluorine'};
+
+  // for(var x in halogens){
+  //   print(x);
+  // }
+
+  var halogens =<String>{};
+  Set <String> names1 ={};
+
+  print (names1.runtimeType);
+
+
+
+
+
 
 
 }
---
+++ file20.dart
@@ -0,0 +1,19 @@
+main (){
+  //map
+
+  // var gifts={
+  //   //key:value
+  //   'first':'partridge',
+  //   'second':'turtledoves',
+  //   'fifth':'golden rings'
+  // };
+// var gifts = Map();
+// gifts['first']='Mango';
+
+
+//   print(gifts['first']);
+
+var gifts = {'first':'Mango','second':'Jack frust'};
+print(gifts['second']);
+
+}
